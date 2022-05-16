cba030c5f9c0f48fe1358d43a3b97320a7275727
diogo464 <diogo464@protonmail.com>
2022-05-16 20:52:59 +0100

    updated ipfs desktop build

--- scripts/build_ipfs_desktop.sh
+++ scripts/build_ipfs_desktop.sh
@@ -27,7 +27,8 @@ fi
 export CSC_IDENTITY_AUTO_DISCOVERY=false
 
 install -D -m 755 bin/ipfs_$OS-$ARCH$EXT third_party/ipfs-desktop/npm-go-ipfs/bin/ipfs$EXT
-docker run --rm -v $(pwd)/third_party/ipfs-desktop/:/project/:Z docker.io/electronuserland/builder:wine sh -c "npm install && npx electron-builder --$EBOS --$EBARCH"
+mkdir -p .cache
+docker run --rm -v $(pwd)/third_party/ipfs-desktop/:/project/:Z docker.io/electronuserland/builder:wine sh -c "npm ci && npm run build && npm install && npx electron-builder --$EBOS --$EBARCH"
 pushd third_party/ipfs-desktop/dist
     find -maxdepth 1 -name '*aarch64*' -type f -exec sh -c 'mv {} $(echo {} | sed "s/aarch64/arm64/")' \; &> /dev/null
     find -maxdepth 1 -name '*x86_64*' -type f -exec sh -c 'mv {} $(echo {} | sed "s/x86_64/amd64/")' \; &> /dev/null
