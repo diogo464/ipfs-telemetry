16494176ae5934eaaae451644f48d52fed574880
diogo464 <diogo464@d464.sh>
2025-04-22 10:02:45 +0100

    clean tree to prepare for reorganization

--- backend/backend/util.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def split_multiaddr_to_peer_and_ip(addr: str) -> tuple[str, str]:
-    match = re.match(r"/ip4/(\d+\.\d+\.\d+\.\d+)/.*?/p2p/(\w+)", addr)
-    if match is None:
-        raise ValueError(f"Invalid multiaddr: {addr}")
-    return match.group(2), match.group(1)
--- backend/webapi/routers/raw.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from fastapi import APIRouter
-from rocketry import Grouper
-
-logger = logging.getLogger(__name__)
-router = APIRouter()
-group = Grouper()
