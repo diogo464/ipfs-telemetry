d82cc90976a302ad830c99f0fef5605f62bb9533
diogo464 <diogo464@d464.sh>
2025-04-27 10:40:13 +0100

    fixed config mounting in victoria metrics container

--- scripts/podman-vm.sh
+++ scripts/podman-vm.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)/..
 mkdir -p data/vm
 podman run -d --name vm --network host \
     -v ./data/vm:/victoria-metrics-data:z \
-    -v ./scripts/scrape.yaml:/etc/scrape.yaml:ro \
+    -v ./scripts/scrape.yaml:/etc/scrape.yaml:ro,z \
     docker.io/victoriametrics/victoria-metrics:latest \
     --selfScrapeInterval=5s -storageDataPath=/victoria-metrics-data \
     -promscrape.config=/etc/scrape.yaml
