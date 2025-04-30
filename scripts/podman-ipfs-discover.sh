0186d74ed345d4e6ad830a3e26c8d51e8faa0d16
diogo464 <diogo464@d464.sh>
2025-04-30 17:08:06 +0100

    scripts: fixed nats subject on podman-ipfs-discover.sh

--- scripts/podman-ipfs-discover.sh
+++ scripts/podman-ipfs-discover.sh
@@ -2,5 +2,5 @@
 set -e
 
 for id in $(podman container list --noheading | grep kubo | cut -d' ' -f1); do
-  podman exec $id ipfs id | nats publish discovery
+  podman exec $id ipfs id | nats publish monitor.discover
 done
